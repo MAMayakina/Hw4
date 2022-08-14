325a43516e354127ad52f2ddae5bd9f9d7914696
Homework3 <laneevama@gmail.com>
2022-08-14 20:15:55 +0300

    Поправила задание 2 (заменила if на switch) и задание 3 (проверки високосного года сделала в одну строчку)

--- src/Main.java
+++ src/Main.java
@@ -4,50 +4,54 @@ public class Main {
         byte clientOS=0;
         //Задание 1
         System.out.println("Задание 1:");
-        if (clientOS==0){
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS==1){
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Входные данные некорректны. Введите 0 или 1");
+        switch(clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Входные данные некорректны. Введите 0 или 1");
+                break;
         }
 
         //Задание 2
         System.out.println("Задание 2:");
         int clientDeviceYear=2014;
-        if (clientOS==0 || clientOS==1) {
-            switch (clientOS) {
-                case 0:
-                    if (clientDeviceYear < 2015) {
-                        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                    } else {
-                        System.out.println("Установите версию приложения для iOS по ссылке");
-                    }
-                    break;
-                case 1:
-                    System.out.print("Установите ");
-                    if (clientDeviceYear < 2015) {
-                        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                    } else {
-                        System.out.println("Установите версию приложения для Android по ссылке");
-                    }
-                    break;
-            }
-        } else {
-            System.out.println("Входные данные некорректны.");
+        switch (clientOS) {
+            case 0:
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                break;
+            case 1:
+                System.out.print("Установите ");
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Входные данные некорректны.");
+                break;
         }
+
         //Задание 3
         System.out.println("Задание 3:");
         int year = 2000;
-        if (year%400==0){
+        if (year % 4 == 0 && year % 100 != 0) {
             System.out.println(year + " год является високосным");
-            } else if (year%100==0){
-            System.out.println(year + " год не является високосным");
-        } else if(year%4==0) {
-            System.out.println(year + " год является високосным");
-        } else{
-            System.out.println(year + " год не является високосным");
         }
+        else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
+            System.out.println(year + " " + "год является високосным");
+        } else
+            System.out.println(year + " год не является високосным");
+
+
         //Задание 4
         System.out.println("Задание 4:");
             int deliveryDistance = 95;
